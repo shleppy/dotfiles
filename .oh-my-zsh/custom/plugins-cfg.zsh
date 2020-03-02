6d02027470da245984fedbd0090871db7bc08f59
Shelby Hendrickx <shelby.hendrickx@hotmail.com>
2020-03-02 20:26:50 +0100

    added plugin settings to separate file

---
+++ .oh-my-zsh/custom/plugins-cfg.zsh
@@ -0,0 +1,11 @@
+# Plugins
+plugins=(
+    artisan
+    colored-man-pages
+    git
+    laravel
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
+
+ARTISAN_OPEN_ON_MAKE_EDITOR=pstorm  # PHPstorm
